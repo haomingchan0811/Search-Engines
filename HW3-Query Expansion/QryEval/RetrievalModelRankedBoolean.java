91dbde67a2add1ed849a669a7d9dc04fbff21e72
Haoming Chen <chenhaomingsz@gmail.com>
2017-03-18 21:49:39 -0400

    finish queryExpansion Class

---
+++ HW3-Query Expansion/QryEval/RetrievalModelRankedBoolean.java
@@ -0,0 +1,19 @@
+/**
+ *  Copyright (c) 2017, Carnegie Mellon University.  All Rights Reserved.
+ */
+
+import java.util.Map;
+
+/**
+ *  An object that stores parameters for the ranked Boolean
+ *  retrieval model (there are none) and indicates to the query
+ *  operators how the query should be evaluated.
+ */
+public class RetrievalModelRankedBoolean extends RetrievalModel {
+
+	public String defaultQrySopName() {
+		return new String("#or");
+	}
+
+	public void setParameters(Map<String, String> param){ return;}
+}
