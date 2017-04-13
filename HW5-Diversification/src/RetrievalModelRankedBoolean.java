71913bb1bcc07718477f5a84a1c4f0f9c37a4f86
Haoming Chen <chenhaomingsz@gmail.com>
2017-04-12 23:36:29 -0400

    Diversification Init

---
+++ HW5-Diversification/src/RetrievalModelRankedBoolean.java
@@ -0,0 +1,28 @@
+/**
+ *  Copyright (c) 2017, Carnegie Mellon University.  All Rights Reserved.
+ */
+
+import java.util.Map;
+
+/**
+ *  An object that stores parameters for the ranked Boolean
+ *  retrieval model (there are none) and indicates to the query
+ *  operators how the query should be evaluated.
+ */
+public class RetrievalModelRankedBoolean extends RetrievalModel {
+
+//	public String defaultQrySopName() {
+//		return new String("#or");
+//	}
+
+	public String defaultQrySopName() {
+		return new String("#or");
+	}
+
+	// change of default operator ("#and") for HW3-query expansion as a baseline for comparison
+//	public String defaultQrySopName() {
+//		return new String("#and");
+//	}
+
+	public void setParameters(Map<String, String> param){ return;}
+}
